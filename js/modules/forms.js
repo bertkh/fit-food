aff169859c79ea54d56134a21e11b1ffdb60bc03
Albert <ber4ify@gmail.com>
2023-05-22 22:42:15 +0300

    add different modules for each functionality

---
+++ js/modules/forms.js
@@ -0,0 +1,83 @@
+import { closeModal, openModal } from "./modal";
+import { postData } from "../services/services";
+
+ // Forms
+
+function forms(formSelector, modalTimerId) {
+
+ const forms = document.querySelectorAll(formSelector);
+
+ const message = {
+   loading: "img/form/spinner.svg",
+   success: "Thanks! We will connect with you soon",
+   error: "Something is wrong",
+ };
+
+ forms.forEach((item) => {
+   bindPostData(item);
+ });
+
+ function bindPostData(form) {
+   form.addEventListener("submit", (e) => {
+     e.preventDefault();
+
+     const statusMessage = document.createElement("img");
+     statusMessage.src = message.loading;
+     statusMessage.style.cssText = `
+       display: block;
+       margin: 0 auto;
+     `;
+     form.insertAdjacentElement("afterend", statusMessage);
+     const formData = new FormData(form);
+
+     const object = {};
+     formData.forEach(function (value, key) {
+       object[key] = value;
+     });
+
+     const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+     postData("http://localhost:3000/requests", json)
+       .then((data) => {
+         console.log(data);
+         showThanksModal(message.success);
+         statusMessage.remove();
+       })
+       .catch(() => {
+         showThanksModal(message.error);
+       })
+       .finally(() => {
+         form.reset();
+       });
+   });
+ }
+
+ function showThanksModal(message) {
+   const prevModalDialog = document.querySelector(".modal__dialog");
+   prevModalDialog.classList.add("hide");
+   openModal('.modal', modalTimerId);
+
+   const thanksModal = document.createElement("div");
+   thanksModal.classList.add(".modal__dialog");
+   thanksModal.innerHTML = `
+     <div class="modal__content">
+       <div class ="modal__close" data-close>×</div>
+       <div class ="modal__title">${message}</div>
+     </div>
+   `;
+
+   document.querySelector(".modal").append(thanksModal);
+   setTimeout(() => {
+     thanksModal.remove();
+     prevModalDialog.classList.add("show");
+     prevModalDialog.classList.remove("hide");
+     closeModal('.modal');
+   }, 4000);
+ }
+
+ fetch("http://localhost:3000/menu")
+   .then((data) => data.json())
+   .then((res) => console.log(res));
+}
+
+export default forms;
